d983c57256485b32ee2b5d168507d88606234713
Paula Lima <paula.lima@rosariosoftware.com>
2020-11-19 15:41:59 -0300

    final visualization configuration

--- todoapp/todolist/urls.py
+++ todoapp/todolist/urls.py
@@ -2,7 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 from todolist.views import index
+from django.contrib.auth.models import User, Group
 
 urlpatterns = [
     path('', index, name='todolist'),
 ]
+
+admin.site.site_header = "DevOps TPI"
+admin.site.index_title = "DevOps Administration | Grupo Nº 1"
+admin.site.site_title = "Grupo 1"
+
+admin.site.unregister(Group)
+admin.site.unregister(User)
