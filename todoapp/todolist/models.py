8d8a19237549f17b59506a86b17d7d10a6925e63
Paula Lima <paula.lima@rosariosoftware.com>
2020-11-20 18:57:00 -0300

    migrations

--- todoapp/todolist/models.py
+++ todoapp/todolist/models.py
@@ -12,7 +12,7 @@ class Category(models.Model): # The Category table name that inherits models.Mod
 
 class TodoList(models.Model): #Todolist able name that inherits models.Model
     title = models.CharField(max_length=250) # a varchar
-    #content = models.TextField(blank=True) # a text field 
+    content = models.TextField(blank=True) # a text field 
     created = models.DateField(default=timezone.now().strftime("%Y-%m-%d")) # a date
     due_date = models.DateField(default=timezone.now().strftime("%Y-%m-%d")) # a date
     category = models.ForeignKey(Category, default="general", on_delete=models.CASCADE) # a foreignkey
